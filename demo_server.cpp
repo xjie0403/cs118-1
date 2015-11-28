473d2907f13cfce41171433a1c6d9619408e08e4
fangdanzai <fangdanzaia@gmail.com>
2015-11-27 18:22:11 -0800

    Update demo_server.cpp

--- demo_server.cpp
+++ demo_server.cpp
@@ -11,27 +11,71 @@
 #include <arpa/inet.h>
 #include "Packet.h"
 #include "Protocol.h"
+#include <string>
+#include <fstream>
+
+#include <streambuf>
 
 using namespace std;
 
-int main() {
+
+
+int main(int argc, char* argv[]) {
+
+	cout<<argc<<endl;
+	for (int n=0; n<5;n++)
+		{
+			cout<<argv[n]<<endl;
+		}
+	if (argc!=5)
+	{	
+		cout<<"Wrong format!"<<endl;
+		return -1;
+	}
+
+
+
 	Protocol obj;
-
-	obj.set_Pl(0.2);
-	obj.set_Pc(0.2);
-
-	cout << obj.set_server(23333) << endl;
-
+	int port;
+	port = atoi(argv[1]);
+	int CW = atoi(argv[2]);
+	string pl = argv[3];
+	string pc = argv[4];
+	//cout<<pl<<' '<<pc<<endl;
+	obj.set_Pl(atof(pl.c_str()));
+	obj.set_Pc(atof(pc.c_str()));
+	
+	cout<<obj.set_server(port)<<endl; 
+	
+	//cout<<"jinlaile wa?"<<endl;
 	while (1) {
 		string filename;
 		obj.rtp_accept(filename);
 		cout << "Filename: " << filename << endl;
-
-		string str = "";
+		
+		string filePath=("./"+filename);
+		
+		ifstream file(filePath.c_str(),ios::in);
+		
+		if(!file)
+		{
+			cout<<"file not found... = =b please check the file name"<<endl;
+			file.close();	
+			return -1;	
+		}
+		istreambuf_iterator<char> beg(file),end;	
+		string bufstr(beg,end);
+		/*
 		for (int i = 0; i < 1000; i++)
 			str = str + "test";
-
-		cout << obj.rtp_send(str) << endl;
+			*/
+		
+		cout << obj.rtp_send(bufstr) << endl;
 		cout << "Done" << endl;
+		
+		
+	
+	
 	}
+	
 }
