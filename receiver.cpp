80d624455b21efe773c50765b445e841628d02ff
renqing <renqing@1992y.com>
2015-11-28 23:28:03 -0800

    help Dan to optimize code

--- receiver.cpp
+++ receiver.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <ctime>
+#include <sstream>
+#include <fstream>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include "Packet.h"
 #include "Protocol.h"
-#include <string>
-#include <sstream>
-#include <fstream>
 
 using namespace std;
 
 
-int main(int argc,char* argv[])
-{
+int main(int argc,char* argv[]) {
 	Protocol obj;
-	
-	cout<<argc<<endl;
-	for (int n=0; n<6;n++)
-		{
-			cout<<argv[n]<<endl;
-		}
-	if (argc!=6)
-	{	
+
+	if (argc != 6) {
 		cout<<"Wrong format!"<<endl;
 		return -1;
 	}
 
-	
 	string hostname;
 	int port;
-	string filename;
-	
+	string filename;	
 
 	hostname = argv[1];
 	port = atoi(argv[2]);
@@ -47,28 +30,22 @@ int main(int argc,char* argv[])
 	obj.set_Pl(atof(pl.c_str()));
 	obj.set_Pc(atof(pc.c_str()));
 	
-	cout<<obj.set_client(hostname,port)<<endl;
+	obj.set_client(hostname,port);
 	
-	
-	//cout<<obj.set_client("127.0.0.1", 23333) << endl
-		cout<<obj.rtp_connect(filename)<<endl;
+	obj.rtp_connect(filename);
 	string message;
-	cout << obj.rtp_read(message) << endl;
-	if (message == "wrong file name!")
-			{
-				cout<<message<<endl;
-			}
-	else{
-	
+	obj.rtp_read(message);
+
+	if (message == "wrong file name!") {
+		cout << message << endl;
+	} else {
 		string filePath=("./"+filename);
 		ofstream file(filePath.c_str());
-	
-		file<<message<<endl;
-	
-	//cout << message << endl;
-	
-	return 0;
+		file << message;
+		file.close();
 	}
+
+	return 0;
 }
 
 
--- sender.cpp
+++ sender.cpp
@@ -1,39 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <ctime>
+#include <fstream>
+#include <streambuf>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include "Packet.h"
 #include "Protocol.h"
-#include <string>
-#include <fstream>
-
-#include <streambuf>
 
 using namespace std;
 
-
-
 int main(int argc, char* argv[]) {
-
-	cout<<argc<<endl;
-	for (int n=0; n<5;n++)
-		{
-			cout<<argv[n]<<endl;
-		}
-	if (argc!=5)
-	{	
+	if (argc != 5) {
 		cout<<"Wrong format!"<<endl;
 		return -1;
 	}
-
-
 
 	Protocol obj;
 	int port;
@@ -41,12 +21,10 @@ int main(int argc, char* argv[]) {
 	int CW = atoi(argv[2]);
 	string pl = argv[3];
 	string pc = argv[4];
-	//cout<<pl<<' '<<pc<<endl;
 	obj.set_Pl(atof(pl.c_str()));
 	obj.set_Pc(atof(pc.c_str()));
 	obj.set_window_size(CW);
-	cout<<obj.set_server(port)<<endl; 
-	
+	obj.set_server(port);
 	
 	while (1) {
 		string filename;
@@ -54,29 +32,20 @@ int main(int argc, char* argv[]) {
 		cout << "Filename: " << filename << endl;
 		
 		string filePath=("./"+filename);
-		
 		ifstream file(filePath.c_str(),ios::in);
 		
-		if(!file)
-		{
-			cout<<"file not found... = =b please check the file name"<<endl;
+		if (!file) {
+			cout << "file not found... = =b please check the file name" << endl;
 			obj.rtp_send("wrong file name!");	
 			continue;
-	
 		}
+
 		istreambuf_iterator<char> beg(file),end;	
 		string bufstr(beg,end);
-		/*
-		for (int i = 0; i < 1000; i++)
-			str = str + "test";
-			*/
-		
-		cout << obj.rtp_send(bufstr) << endl;
-		cout << "Done" << endl;
-		
-		
-	
-	
+		obj.rtp_send(bufstr);
+
+		cout << "Transfered successfully" << endl;
 	}
 	
+	return 0;
 }
